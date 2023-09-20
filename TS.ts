96b78bd3f182cd3dc117a1818bb88040a965e83b
Jairo TJ <jairotjunior@hotmail.com>
2023-09-20 07:39:43 -0300

    Duas funções, e o uso do Typescript

---
+++ TS.ts
@@ -0,0 +1,10 @@
+function multiplicacao(x: number, y:number){
+    return x*y;
+}
+
+function saudacao(nome: string){
+    return `Olá ${nome}`;
+}
+
+console.log(multiplicacao(2,5));
+console.log(saudacao("Jairo"));
