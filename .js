069f979608ac51631fc00c9439511a5f0716f60a
Jairo TJ <jairotjunior@hotmail.com>
2023-10-03 11:10:34 -0300

    Envio de Exercício

--- .js
+++ .js
@@ -1,36 +1,43 @@
-class aluno {
-    constructor(nome, nota){
-        this.nome = nome;
-        this.nota = nota;
-    }
+// class aluno {
+//     constructor(nome, nota){
+//         this.nome = nome;
+//         this.nota = nota;
+//     }
 
-    mediaNotas(){
-        if (this.nota >= 6){
-            console.log(`Aluno ${this.nome} foi aprovado com média ${this.nota}`);
-        }else{
-            console.log(`Aluno ${this.nome} foi reprovado com média ${this.nota}`);
-        }
-    }
-}
+//     mediaNotas(){
+//         if (this.nota >= 6){
+//             console.log(`Aluno ${this.nome} foi aprovado com média ${this.nota}`);
+//         }else{
+//             console.log(`Aluno ${this.nome} foi reprovado com média ${this.nota}`);
+//         }
+//     }
+// }
 
-class sala1 extends aluno{
+// class sala1 extends aluno{
 
-}
+// }
 
-const jairo = new sala1("Jairo", 5);
-jairo.mediaNotas();
+// const jairo = new sala1("Jairo", 5);
+// jairo.mediaNotas();
 
 
 // function media(notas){
 //     return 
 // }
 
-// notas.forEach(function(nota){
+let notas = [6, 10, 5, 3];
+let soma = 0;
 
-//     let notas = [6, 10, 5, 3];
-//     let soma = 0;
-//     soma += nota;
+notas.forEach(function(nota){
+    soma += nota;
+});
 
-// });
+function calcMedia(soma){
+    return soma/notas.length;
+}
 
-// console.log(`\nMédia: ${soma/notas.length}`);
+const aprovados = notas.filter(function(media) {
+    return calcMedia(soma);
+})
+
+console.log(aprovados);
