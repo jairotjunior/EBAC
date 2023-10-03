bef64cafb48a69451368262a45272154303b9ba7
Jairo TJ <jairotjunior@hotmail.com>
2023-10-03 11:14:21 -0300

    Envio de Exercício

--- .js
+++ .js
@@ -1,43 +1,23 @@
-// class aluno {
-//     constructor(nome, nota){
-//         this.nome = nome;
-//         this.nota = nota;
-//     }
 
-//     mediaNotas(){
-//         if (this.nota >= 6){
-//             console.log(`Aluno ${this.nome} foi aprovado com média ${this.nota}`);
-//         }else{
-//             console.log(`Aluno ${this.nome} foi reprovado com média ${this.nota}`);
-//         }
-//     }
-// }
+const alunos = [
+    {
+        nome: "Jairo",
+        notas: 6
+    },
 
-// class sala1 extends aluno{
+    {
+        nome: "Pedro",
+        notas: 5
+    },
 
-// }
+    {
+        nome: "Manoel",
+        notas: 10
+    }
+]
 
-// const jairo = new sala1("Jairo", 5);
-// jairo.mediaNotas();
-
-
-// function media(notas){
-//     return 
-// }
-
-let notas = [6, 10, 5, 3];
-let soma = 0;
-
-notas.forEach(function(nota){
-    soma += nota;
-});
-
-function calcMedia(soma){
-    return soma/notas.length;
-}
-
-const aprovados = notas.filter(function(media) {
-    return calcMedia(soma);
+const aprovados = alunos.filter((nota)=> {
+    return nota.notas >= 6;
 })
 
 console.log(aprovados);
